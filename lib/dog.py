187ba251aa1209ef2bd181d1a66010ff77d7b339
joshua chivile <joshuachivile@gmail.com>
2023-11-29 16:38:18 +0300

    Instance methods in python

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    #Instance method definition what this dog can do
+    def bark(self): # needed with instance methods ; // method --> function within a class
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
